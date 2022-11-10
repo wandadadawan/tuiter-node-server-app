43de1224f135a70698d8082abfb1c928125e5fbc
Wanda Wang <chuwang@wustl.edu>
2022-11-09 20:46:33 -0800

    Completed Section 3

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import HelloController from "./controllers/hello-controller.js";
+import UsersController from "./controllers/users/users-controller.js";
 const app = express();
-app.get('/hello', (req, res) => {res.send('Life is good!')});
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')});
+HelloController(app);
+UsersController(app);
 app.listen(4000);
---
+++ controllers/users/users-controller.js
@@ -0,0 +1,25 @@
+import people from './users.js';
+
+let users = people;
+const UserController = (app) => {
+  app.get('/api/users', findUsers);
+  app.get('/api/users/:uid', findUserById);
+};
+
+const findUsers = (req, res) => {
+  const type = req.query.type; // can also use req.query['type']
+  if (type) {
+    const usersOfType = users.filter(u => u.type === type);
+    res.json(usersOfType);
+    return;
+  }
+  res.json(users); // return a json data of all users
+};
+
+const findUserById = (req, res) => {
+  const userId = req.params.uid;
+  const user = users.find(u => u._id === userId);
+  res.json(user);
+}
+
+export default UserController;
