09426121221f91ba99d35e208d33025d12f44ba0
Wanda Wang <chuwang@wustl.edu>
2022-11-10 11:28:56 -0800

    Completed section 4

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
 const app = express();
+app.use(express.json()); // parse JSON from HTTP request body
 HelloController(app);
 UsersController(app);
 app.listen(4000);
