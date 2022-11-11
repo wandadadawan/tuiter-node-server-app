403f9635aab55d3a2b5174f6373d1c6fb1acf97f
Wanda Wang <chuwang@wustl.edu>
2022-11-11 13:09:50 -0800

    Add one additional post

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
+
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 const app = express();
+app.use(cors()); // allow cross origin sharing
 app.use(express.json()); // parse JSON from HTTP request body
 TuitsController(app);
 HelloController(app);
