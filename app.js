4dba6dc6c4425bd693eff6ccfebf21ac54737300
Wanda Wang <chuwang@wustl.edu>
2022-11-11 16:44:10 -0800

    Change Port config for Heroku

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-
-
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
@@ -12,4 +10,4 @@ app.use(express.json()); // parse JSON from HTTP request body
 TuitsController(app);
 HelloController(app);
 UsersController(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
